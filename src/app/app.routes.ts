9d9c7d299f5ae751f4a9d265a2d5d7f522bcc9ea
safarizkyfaddillah <148610387+safarizkyfaddillah@users.noreply.github.com>
2026-02-19 20:54:53 +0700

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    // Perhatikan bagian /:id -> ini adalah parameter dinamis
+    path: 'detail/:id',
+    loadComponent: () => import('./detail/detail.page').then( m => m.DetailPage)
+  },  {
+    path: 'tambah-mhs',
+    loadComponent: () => import('./tambah-mhs/tambah-mhs.page').then( m => m.TambahMhsPage)
+  },
+
+];
